a7a474307ab84a337c570018ccffa09924ad39f3
Erick Varela <48369338+varelaerick@users.noreply.github.com>
2020-11-17 09:42:48 -0300

    Update correios_api.py
    The correios BuscaCep website has changed and I fixed the code to keep scraping.
    
    Basically, I changed request.post to requests.get and builded new ULR

--- consulta_correios/correios_api.py
+++ consulta_correios/correios_api.py
@@ -5,40 +5,34 @@ import unidecode
 import re
 
 
-def chunks(l, n):
-    n = max(1, n)
-    return list(l[i:i+n] for i in range(0, len(l), n))
-
-
 def busca_cep(data_info):
 
     # Getting data
     data_info = normalize('NFKD', data_info).encode(
         'ASCII', 'ignore').decode('ASCII')
     session = requests.session()
-    data = {'relaxation': data_info,
-            'TipoCep': 'ALL',
-            'semelhante': 'N',
+    
+    data = {
+        'endereco': data_info.replace(' ','%20'),
             }
+    # URl sending the string parameters: 
+    #'pagina = %2Fapp%2Fendereco%2Findex.php','tipoCEP = ALL' and 'endereco=data'
+    
+    url = ('https://buscacep.correios.com.br/app/endereco/carrega-cep-endereco.php?pagina%2Fapp%2Fendereco%2Findex.php&cepaux=&mensagem_alerta=&endereco={endereco}&tipoCEP=ALL'.format(**data))
+    
+    r = session.get(url)
 
-    r = session.post(
-        "http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data)
-
-    content = r.content
-
-    # Parsing
-    soup = BeautifulSoup(content, features="html.parser")
-    content = soup.find_all('table')
+    content = r.json()
 
     if content:
         data = []
-        items = content[0].find_all('td')
-        for info in chunks(items, 4):
+        items = content['dados']
+        for info in items:
             data.append({
-                'address': unidecode.unidecode(re.sub('<.*?>', '', str(info[0])).strip()),
-                'neighborhood': unidecode.unidecode(info[1].string.strip()),
-                'city/state': unidecode.unidecode(info[2].string.strip()),
-                'zipcode': unidecode.unidecode(info[3].string.strip()),
+                'address': unidecode.unidecode(re.sub('<.*?>', '', str(info['logradouroDNEC'])).strip()),
+                'neighborhood': unidecode.unidecode(info['bairro'].strip()),
+                'city/state': unidecode.unidecode(info['localidade']+'/'+info['uf'].strip()),
+                'zipcode': unidecode.unidecode(info['cep'].strip()),
             })
 
     else:
@@ -46,3 +40,5 @@ def busca_cep(data_info):
 
     # Returning data
     return data
+
+
